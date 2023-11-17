f03f07a2e263d070bf350ec6d53262380d1ca033
Daria Mandziuk <dariacolorso@gmail.com>
2023-11-17 16:31:05 +0400

    add first screen

--- app/src/main/java/com/mandarin/andersenapplication/MainActivity.kt
+++ app/src/main/java/com/mandarin/andersenapplication/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.mandarin.andersenapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mandarin.andersenapplication.R.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+
     }
 }
