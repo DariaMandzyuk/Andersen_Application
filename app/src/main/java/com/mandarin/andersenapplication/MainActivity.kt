62afe7ffd61d503fdfd0d696a42419ec89cd4c7d
Daria Mandziuk <dariacolorso@gmail.com>
2023-11-17 16:33:51 +0400

    add changes to mainActivity

--- app/src/main/java/com/mandarin/andersenapplication/MainActivity.kt
+++ app/src/main/java/com/mandarin/andersenapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.mandarin.andersenapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.mandarin.andersenapplication.R.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        val button = findViewById<Button>(id.open_next_page)
+        button.setOnClickListener {
+            Toast.makeText(this, "Кнопка была нажата", Toast.LENGTH_SHORT).show()
+        }
     }
 }
